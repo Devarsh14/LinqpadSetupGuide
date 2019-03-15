d3fe28c86ce06ceee857bb176165c8b9f9a83388
Devarsh shukla <Devarsh14@gmail.com>
2019-03-15 23:08:48 +1000

    7.0 Change connection string implementation in class library's DBcontext. 8.0 Provide connection to ClassLibrary DLL after building project to the Linqpad it shall work.

--- LinqPadClassLibrary/Models/NorthwindContext.cs
+++ LinqPadClassLibrary/Models/NorthwindContext.cs
@@ -33,11 +33,15 @@ namespace LinqPadClassLibrary.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
+                string _connString = "Data Source=DESKTOP-PPL5CRC\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True;";
+                if (!string.IsNullOrWhiteSpace(_connString))
+                {
+                    optionsBuilder.UseSqlServer(_connString);
+                }
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-PPL5CRC\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True;");
+
             }
         }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("ProductVersion", "2.2.3-servicing-35854");
