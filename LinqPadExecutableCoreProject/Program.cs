0c25df344f5ef57239737653e2ce981b2e772be2
Devarsh shukla <Devarsh14@gmail.com>
2019-03-15 22:55:04 +1000

    3. 0 add console or webapplication as executable project as ClassLibrary project can not be executated in Isolation. you will get  There is no runtime associated with this framework, and projects targeting it cannot be executed directly. To use the Entity Framework Core Package Manager Console Tools with this project, add an executable project targeting .NET Framework or .NET Core that references this project, and set it as the startup project; or, update this project to cross-target .NET Framework or .NET Core. For more information on using the EF Core Tools with .NET Standard projects, see https://go.microsoft.com/fwlink/?linkid=2034705
    So remove this error add new project console here. and set reference to class library.

---
+++ LinqPadExecutableCoreProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqPadExecutableCoreProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
